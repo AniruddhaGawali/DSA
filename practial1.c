ff23cd2b1cf5e690c7bd2c81fb8a00658dd3c93e
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2022-11-23 07:18:28 +0000

    added bianry and sort array

--- practial1.c
+++ practial1.c
@@ -8,16 +8,54 @@ int LinearSearch(int *arr, int key)
     return 0;
 }
 
-int BinarySearch(int *arr,int key){
-    
+int BianarySearch(int *arr, int key, int len)
+{
+    int start = 0, end = len;
+
+    while (start <= end)
+    {
+        int mid = (end + start) / 2;
+        if (arr[mid] == key)
+        {
+            return mid + 1;
+        }
+        else
+        {
+            if (arr[mid] > key)
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid;
+            }
+        }
+    }
     return 0;
+}
+
+void Sort(int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main()
 {
     // menu
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    printf("\n%d\n",LinearSearch(arr, 1));
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    
 
     return 0;
 }
