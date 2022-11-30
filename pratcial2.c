988fc383150fe968faf93224b08acacd3f044cab
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2022-11-30 06:52:25 +0000

    added selection sort

--- pratcial2.c
+++ pratcial2.c
@@ -1,13 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int BubbleSort(int *arr,int len)
+int printArray(int *arr, int len)
 {
-    for(int i =0; i<len;i++)
+    for (int i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
+
+int BubbleSort(int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
     {
-        for(int j =0;j<len-1;j++)
+        printArray(arr, len);
+        for (int j = 0; j < len - 1; j++)
         {
-            if(arr[j]>arr[j+1]){
-                 int temp = arr[j];
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -16,32 +26,41 @@ int BubbleSort(int *arr,int len)
     return 0;
 }
 
-
-int SelectionSort()
+int SelectionSort(int *arr, int len)
 {
-    
+    int min, temp;
+    for (int i = 0; i < len; i++)
+    {
+        min = i;
+        printArray(arr, len);
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        
+    }
+
     return 0;
 }
 
 
-int printArray(int * arr, int len)
-{
-    for(int i =0;i<len;i++)
-        printf("%d ",arr[i]);
-    printf("\n");
-    return 0;
-}
+
 
 
 int main()
 {
 
-    int arr[]={7,3,6,2,1,5};
-    printArray(arr, sizeof(arr)/4);
-    BubbleSort(arr,sizeof(arr)/4);
+    int arr[] = {7, 3, 6, 2, 1, 5};
+    SelectionSort(arr, sizeof(arr) / 4);
     printf("\n\n");
-    printArray(arr, sizeof(arr)/4);
+    int arr1[] = {7, 3, 6, 2, 1, 5};
+    BubbleSort(arr1, sizeof(arr1) / 4);
 
-    
     return 0;
 }
